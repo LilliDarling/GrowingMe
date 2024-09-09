96d8f540686faec4de1467872c3c31db1406ffe1
LilliDarling <valkyrieremedy@gmail.com>
2024-09-09 09:46:53 -0600

    Queries error handling

--- api/utils/exceptions.py
+++ api/utils/exceptions.py
@@ -1,4 +1,12 @@
 from fastapi import HTTPException
+from pymongo.errors import PyMongoError, DuplicateKeyError
 
 
-category_exception = HTTPException(status_code=404, detail="Can't be found!")
+def handle_not_found_error(detail: str = "Item not found"):
+  raise HTTPException(status_code=404, detail=detail)
+
+
+def handle_pymongo_error(e: PyMongoError):
+  if isinstance(e, DuplicateKeyError):
+    raise HTTPException(status_code=400, detail="Duplicate key error")
+  raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
