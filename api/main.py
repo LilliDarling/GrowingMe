9f6fa2a9cfd4627507a681c5148d4efac2e89b8d
Lillith Long <valkyrieremedy@gmail.com>
2024-10-19 15:39:14 -0600

    Updated main.py

--- api/main.py
+++ api/main.py
@@ -1,9 +1,8 @@
-from routes import post, chapter, category
+from routes import post, category
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
 app.include_router(post.router, tags=["Post"])
-app.include_router(chapter.router, tags=["Chapter"])
 app.include_router(category.router, tags=["Category"])
