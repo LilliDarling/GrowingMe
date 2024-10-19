59255a80d52be0fbbe37ab2e22fef619bf591db5
Lillith Long <valkyrieremedy@gmail.com>
2024-10-19 14:33:02 -0600

    Post Models built

--- api/models/category.py
+++ api/models/category.py
@@ -1,22 +1,19 @@
 from odmantic import Model, Field
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 
 class CategoryIn(Model):
     name: str = Field(unique=True)
     is_active: bool = True
 
-
 class CategoryOut(Model):
     name: str
     is_active: bool = True
 
-
 class CategoryList(BaseModel):
     categories: List[CategoryOut]
 
-
 class CategoryPatchSchema(BaseModel):
-    name: str = None
-    is_active: bool = None
+    name: Optional[str] = None
+    is_active: Optional[bool] = None
