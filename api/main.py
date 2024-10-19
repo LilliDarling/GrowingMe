d1c125cc1cd8feb7291b8932b403aaf7be076fe2
Lillith Long <valkyrieremedy@gmail.com>
2024-10-19 13:54:40 -0600

    Removed code

--- api/main.py
+++ api/main.py
@@ -7,14 +7,3 @@ app = FastAPI()
 app.include_router(post.router, tags=["Post"])
 app.include_router(chapter.router, tags=["Chapter"])
 app.include_router(category.router, tags=["Category"])
-
-
-# @app.on_event("startup")
-# async def startup():
-# 	try:
-# 		await initialize_database()
-# 		print("database connected")
-# 	except Exception as e:
-# 		print(f"Unable to connect database: {e}")
-# 		import sys
-# 		sys.exit(1)
