444b7697ba319d10a66002ead2197333d2b31f53
Lillith Long <valkyrieremedy@gmail.com>
2024-11-11 12:14:57 -0700

    Reconfigured dependencies and config files

--- expo-app/babel.config.js
+++ expo-app/babel.config.js
@@ -3,7 +3,30 @@ module.exports = function (api) {
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+      "nativewind/babel"
     ],
+    plugins: 
+      [
+        ['module-resolver', {
+          root: ['./'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json'
+          ],
+          alias: {
+            '@': './',
+            '@components': './components',
+            '@screens': './screens',
+            '@utils': './utils'
+          }
+        }],
+      ]
   };
 };
