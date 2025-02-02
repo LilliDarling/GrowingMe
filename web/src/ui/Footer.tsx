2317eebf69d6afe48a9ae43bcd20ca247313eff4
LilliDarling <valkyrieremedy@gmail.com>
2025-02-02 10:50:17 -0700

    Organized src tree

---
+++ web/src/ui/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        Footer stuff
+      </div>
+    </footer>
+  )
+}
---
+++ web/src/ui/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        Navbar
+      </div>
+    </nav>
+  )
+}
